e30e0aa3b9f254c4b5c02a8b7adfaf69c784a2bc
teoderboven <teoderboven@outlook.com>
2023-07-15 20:00:25 +0200

    Create Footer

--- mainassets/navbar.js
+++ mainassets/navbar.js
@@ -1,18 +1,12 @@
-var header = document.querySelector("header");
-var nav = document.querySelector("header nav");
-var menuBtn = document.getElementById("menuBtn");
+var header = document.querySelector('header');
+var nav = document.querySelector('header nav');
+var menuBtn = document.getElementById('menuBtn');
 
-function switchNavBar(){
-	if(nav.classList.contains('visible')){
-		nav.classList.remove('visible');
-	}else{
-		nav.classList.add('visible');
-	}
-}
-
-menuBtn.addEventListener('click', switchNavBar);
+menuBtn.addEventListener('click', ()=>{
+	nav.classList.toggle('visible');
+});
 document.addEventListener('click', (e)=>{ // close navbar if click anywhere
 	if(!header.contains(e.target)){
 		nav.classList.remove('visible')
 	}
-})
+});
