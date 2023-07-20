fc960e5efa599aa66f90bf7d5fbe18a13666c632
teoderboven <teoderboven@outlook.com>
2023-07-20 22:25:05 +0200

    sortie: case of past date and today’s date

--- sortie/assets/sortie.js
+++ sortie/assets/sortie.js
@@ -3,18 +3,52 @@ if(dateElt){
 	const calendar = dateElt.querySelector("span");
 	const time = dateElt.querySelector("time");
 
-	var date = new Date(time.dateTime);
+	var eventDate = new Date(time.dateTime);
 
-	if(isNaN(date)){ // invalid date
+	/**
+	 * Checks if a date is today
+	 * @param {Date} date the date to check
+	 * @return {boolean} true if the date is today, false else 
+	 */
+	function isToday(date){
+		if(!(date instanceof Date)) return false;
+
+		const today = new Date();
+
+		return date.getDate() === today.getDate() &&
+		date.getMonth() === today.getMonth() &&
+		date.getFullYear() === today.getFullYear();
+	}
+
+	if(isNaN(eventDate)){ // invalid date
 		dateElt.style.display = 'none';
 	}
 
-	time.textContent = `${date.toLocaleDateString("fr-BE",{
-		weekday: 'long',
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric'
-	})} à ${date.getHours()}h${date.getMinutes() > 0? new String(date.getMinutes()).padStart(2, '0'):''}`; // display minutes on 2 digits if not zero
+	if(eventDate.getTime() < Date.now()){ // past event
+		time.textContent = "Cet événement est passé";
+		time.classList.add('past');
+		calendar.style.display = 'none';
+		dateElt.querySelector('#calendarContainer').style.display = 'none';
+	}else{
+		time.innerHTML = `${
+			isToday(eventDate)?
+			"Aujourd'hui" // event is today
+			: eventDate.toLocaleDateString("fr-BE",{ // event is not today
+			weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric'
+		})} à ${
+			eventDate.getHours()
+		}h${
+			eventDate.getMinutes() > 0? new String(eventDate.getMinutes()).padStart(2, '0'):'' // display minutes on 2 digits if not zero
+		}${
+			time.dataset.duration? ` (dure &#8776; ${time.dataset.duration})`:'' // display event duration
+		}`; 
 
-	calendar.textContent = date.getDate();
+		calendar.textContent = eventDate.getDate();
+
+		
+	}
+	
 }
