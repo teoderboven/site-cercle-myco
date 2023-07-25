324f5dbaed8de182eb72d4d20e77e6e3923fe632
teoderboven <teoderboven@outlook.com>
2023-07-25 23:34:21 +0200

    sortie: add meteo widget

--- sortie/assets/sortie.js
+++ sortie/assets/sortie.js
@@ -1,35 +1,58 @@
-const dateElt = document.getElementById("date");
-if(dateElt){
-	const calendar = dateElt.querySelector("span");
-	const time = dateElt.querySelector("time");
+/**
+ * Checks if a date is today
+ * @param {Date} date the date to check
+ * @return {boolean} true if the date is today, false else 
+ */
+function isToday(date){
+	if(!(date instanceof Date)) return false;
 
-	var eventDate = new Date(time.dateTime);
+	const today = new Date();
 
-	/**
-	 * Checks if a date is today
-	 * @param {Date} date the date to check
-	 * @return {boolean} true if the date is today, false else 
-	 */
-	function isToday(date){
-		if(!(date instanceof Date)) return false;
+	return date.getDate() === today.getDate() &&
+	date.getMonth() === today.getMonth() &&
+	date.getFullYear() === today.getFullYear();
+}
+/**
+ * Calculates the number of days between 2 dates (excluding hours)
+ * @param {Date} startDate the start date
+ * @param {Date} endDate the end date
+ * @returns {number} the number of days between the 2 dates or null if a parameter is not a date
+ */
+function getDaysDifference(startDate, endDate){
+	if(!(startDate instanceof Date) || !(endDate instanceof Date)) return null;
+	
+	var start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+	var end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+	
+	return Math.ceil((end - start) / (1000 * 3600 * 24));
+}
+/**
+ * main function of this file
+ */
+function _sortie(){
+	const dateElt = document.getElementById("date");
+	const meteoElt = document.getElementById("meteo");
 
-		const today = new Date();
+	// date
+	if(dateElt){
+		const calendar = dateElt.querySelector("span");
+		const time = dateElt.querySelector("time");
 
-		return date.getDate() === today.getDate() &&
-		date.getMonth() === today.getMonth() &&
-		date.getFullYear() === today.getFullYear();
-	}
+		const eventDate = new Date(time.dateTime);
 
-	if(isNaN(eventDate)){ // invalid date
-		dateElt.style.display = 'none';
-	}
+		if(isNaN(eventDate)){ // invalid date
+			dateElt.style.display = 'none';
+		}
 
-	if(eventDate.getTime() < Date.now()){ // past event
-		time.textContent = "Cet événement est passé";
-		time.classList.add('past');
-		calendar.style.display = 'none';
-		dateElt.querySelector('#calendarContainer').style.display = 'none';
-	}else{
+		if(eventDate.getTime() < Date.now()){ // past event
+			time.textContent = "Cet événement est passé";
+			time.classList.add('past');
+			calendar.style.display = 'none';
+			dateElt.querySelector('#calendarContainer').style.display = 'none';
+
+			return;
+		}
+
 		time.innerHTML = `${
 			isToday(eventDate)?
 			"Aujourd'hui" // event is today
@@ -47,8 +70,55 @@ if(dateElt){
 		}`; 
 
 		calendar.textContent = eventDate.getDate();
-
 		
+		// meteo  -- linked with date
+		if(meteoElt){
+			if(!meteoElt.dataset.cityurl){
+				meteoElt.style.display = 'none';
+
+				return;
+			}
+
+			const meteoReq = new XMLHttpRequest();
+			meteoReq.onreadystatechange = ()=>{
+				if(meteoReq.readyState === XMLHttpRequest.DONE && meteoReq.status === 200){
+					const res = JSON.parse(meteoReq.responseText);
+
+					if(res.errors){
+						meteoElt.style.display = 'none';
+						return;
+					}
+
+					const daysRemaining = getDaysDifference(new Date(), eventDate);
+					console.table({'Calculated days remaining': daysRemaining});
+					
+					if(daysRemaining <= 4){
+						const fcst = res[`fcst_day_${daysRemaining}`].hourly_data[`${eventDate.getHours()}H00`];
+						console.table(fcst);
+
+						const img = document.createElement('img');
+						let icon = fcst.ICON;
+						icon = icon.slice(0, icon.indexOf('.png')) + '-big.png'; // transform icon into big icon (other url)
+						img.src = icon;
+
+						const tmp = document.createElement('span');
+						tmp.classList.add('tmp');
+						tmp.innerHTML = `${Math.round(fcst.TMP2m)}&deg;C`;
+
+						const cond = document.createElement('span');
+						cond.classList.add('cond');
+						cond.textContent = fcst.CONDITION;
+
+						meteoElt.appendChild(img);
+						meteoElt.appendChild(tmp);
+						meteoElt.appendChild(cond);
+						meteoElt.classList.add('ready');
+					}
+				}
+			}
+			meteoReq.open('GET', `https://prevision-meteo.ch/services/json/${meteoElt.dataset.cityurl}`);
+			meteoReq.send();
+		}
 	}
-	
 }
+_sortie();
