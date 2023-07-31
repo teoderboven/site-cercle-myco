a67589f217f4667b405299ece9b1d1b46f596df7
teoderboven <teoderboven@outlook.com>
2023-07-31 23:49:56 +0200

    Adding image viewer

--- mainassets/js/images.js
+++ mainassets/js/images.js
@@ -1,3 +1,16 @@
 Array.from(document.getElementsByClassName("image")).forEach(elt => {
 	elt.addEventListener('contextmenu', (e)=> e.preventDefault());
 });
+
+// import viewer if necessary
+if(document.querySelector('.image.viewable')){
+	const css = document.createElement('link');
+	css.rel = "stylesheet";
+	css.href = "/mainassets/css/viewer.css";
+
+	const script = document.createElement('script');
+	script.src = "/mainassets/js/viewer.js";
+
+	document.head.appendChild(css);
+	document.head.appendChild(script);
+}
