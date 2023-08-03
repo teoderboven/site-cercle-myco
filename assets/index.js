f3eb597faaa7dae44ca22b3f5608c8562373a86c
teoderboven <teoderboven@outlook.com>
2023-08-04 01:47:07 +0200

    Index: add top carousel

---
+++ assets/index.js
@@ -0,0 +1,103 @@
+// top carousel
+{
+	const topCarousel = document.getElementById('topCarousel');
+	const carousel = topCarousel.querySelector('.carousel');
+	const carouselItems = topCarousel.querySelectorAll('.item');
+	const dots = topCarousel.querySelectorAll('.dot');
+	let currentIndex = 0;
+	let isDragging = false;
+	let startPointX = 0;
+	let startPointY = 0
+	let moveX = 0;
+
+	/**
+	 * Shows the specified carousel item in the carousel container
+	 * @param {number} index The index of the carousel item to show
+	 */
+	function showCarouselItem(index){
+		carousel.style.transform = `translateX(-${index * 100}%)`;
+		carouselItems.forEach(item => item.classList.remove('active'));
+		dots.forEach(dot => dot.classList.remove('active'));
+		dots[currentIndex].classList.add('active');
+	}
+
+	showCarouselItem(currentIndex);
+
+	let carouselInterval = setInterval(() => {
+		currentIndex = (currentIndex + 1) % carouselItems.length;
+		showCarouselItem(currentIndex);
+	}, 5000);
+	
+	dots.forEach((dot, index)=>{
+		dot.addEventListener('click', ()=>{
+			currentIndex = index;
+			showCarouselItem(currentIndex);
+			if(carouselInterval){
+				clearInterval(carouselInterval);
+				carouselInterval = undefined;
+			}
+		});
+	});
+
+	// dragging items
+
+	/**
+	 * Handles the start of dragging the carousel with mouse or touch
+	 * @param {Event} event The mousedown or touchstart event object
+	 */
+	function handleDragStart(event){
+		isDragging = true;
+		startPointX = event.clientX || event.touches[0].clientX;
+ 		startPointY = event.clientY || event.touches[0].clientY;
+	}
+	/**
+	 * Handles the dragging of the carousel with mouse or touch
+	 * @param {Event} event The mousemove or touchmove event object
+	 */
+	function handleDragMove(event){
+		if(isDragging){
+			const clientX = event.clientX || event.touches[0].clientX;
+			const clientY = event.clientY || event.touches[0].clientY;
+			const deltaX = clientX - startPointX;
+			const deltaY = clientY - startPointY;
+		
+			// Check if the swipe is more vertical than horizontal
+			if(Math.abs(deltaY) > Math.abs(deltaX)){
+				// Allow default scrolling behavior for vertical swipe
+				return;
+			}
+			// Horizontal swipe
+			moveX = deltaX;
+			carousel.style.transition = 'none';
+			carousel.style.transform = `translateX(calc(-${currentIndex * 100}% + ${moveX}px))`;
+		
+			if(carouselInterval){
+				clearInterval(carouselInterval);
+				carouselInterval = undefined;
+			}
+		  }
+	}
+	/**
+	 * Handles the end of dragging the carousel with mouse or touch.
+	 * @param {Event} event The mouseup or touchend event object.
+	 */
+	function handleDragEnd(event){
+		if(isDragging){
+			isDragging = false;
+			carousel.style.transition = '';
+			if(Math.abs(moveX) > 50){
+				currentIndex += moveX > 0 ? -1 : 1;
+				currentIndex = Math.min(Math.max(currentIndex, 0), carouselItems.length - 1);
+			}
+			moveX = 0;
+			showCarouselItem(currentIndex);
+		}
+	}
+	carousel.addEventListener('mousedown', handleDragStart);
+	document.addEventListener('mousemove', handleDragMove);
+	document.addEventListener('mouseup', handleDragEnd);
+	carousel.addEventListener('touchstart', handleDragStart);
+	document.addEventListener('touchmove', handleDragMove);
+	document.addEventListener('touchend', handleDragEnd);
+}
+
