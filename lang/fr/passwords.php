b336871dcd23f38e02229852e6cbe874dcb9e787
teoderboven <teoderboven@outlook.com>
2024-08-24 17:24:09 +0200

    Localization: publish french app locale

---
+++ lang/fr/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+	https://github.com/s-damian/laravel-lang-fr
+*/
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	'reset' => 'Mot de passe réinitialisé',
+	'sent' => 'Un email contenant un lien de réinitialisation du mot de passe a été envoyé',
+	'throttled' => 'Veuillez patienter avant de réessayer',
+	'token' => 'Le jeton de réinitialisation du mot de passe n\'est pas valide',
+	'user' => 'Aucun utilisateur n\'a été trouvé avec cette adresse e-mail',
+
+];
