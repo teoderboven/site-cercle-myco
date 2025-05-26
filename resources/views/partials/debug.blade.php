f93c09d8a1d68cf5600847ad204f486c37c4290f
Téo Derboven <teoderboven@outlook.com>
2025-05-26 17:10:55 +0200

    Update to Laravel 12

--- resources/views/partials/debug.blade.php
+++ resources/views/partials/debug.blade.php
@@ -1,3 +1,5 @@
 <div style="background-color: red; color: black; opacity: 0.5; padding: 7px; border-radius: 7px; position:fixed; right: 15px; bottom: 15px; pointer-events:none; z-index:9999999999">
-	Debug Mode
+	<p style="font-weight: bold;text-align:center">Debug Mode</p>
+	<p style="font-size: .7em">PHP Version: {{ phpversion() }}</p>
+	<p style="font-size: .7em">Laravel Version: {{ app()->version() }}</p>
 </div>
