b0f6549caf866914557ab2223db4b4277d36bbb1
Téo Derboven <teoderboven@outlook.com>
2026-08-15 16:08:34 +0200

    Activities: update modal + notify-btn texts

--- resources/views/partials/activity/notify-button.blade.php
+++ resources/views/partials/activity/notify-button.blade.php
@@ -2,12 +2,15 @@
 
 {{-- Component notification button --}}
 <div class="notify-btn-wrapper">
+    @php
+        $notSubscribedText = "Recevoir des notifications"
+    @endphp
     <button
             type="button"
             class="cmb-btn notify-btn"
             data-activity-id="{{ $activityId }}"
             data-activity-title="{{ $activityTitle }}"
-            data-not-subscribed-text="M'informer par e-mail"
+            data-not-subscribed-text="{{ $notSubscribedText }}"
             data-subscribed-text="Inscrit(e) aux notifications"
             data-subscribed="false"
             title="Tenez-vous informé des mises à jour de cette activité par e-mail"
@@ -16,7 +19,7 @@
         <span class="icon loading"></span>
         <span class="icon check"></span>
         <span class="icon mail"></span>
-        <span class="notify-text">M'informer par e-mail</span>
+        <span class="notify-text">{{ $notSubscribedText }}</span>
     </button>
     <div class="status-message-wrapper">
         <span class="status-message" aria-live="polite"></span>
--- resources/views/partials/activity/subscription-mail-modal.blade.php
+++ resources/views/partials/activity/subscription-mail-modal.blade.php
@@ -2,6 +2,13 @@
     <form method="dialog" class="modal-content" id="subscription-mail-form">
         <h1>S'inscrire aux notifications de l'activité <span class="activity-title"></span></h1>
 
+        <div class="modal-description-container">
+            <p class="modal-description">
+                Recevez des <span class="highlight">rappels e-mail</span> pour ne pas manquer l'activité, ainsi que toute information importante <span class="highlight">en cas d'imprévu</span>.
+                L'inscription aux notifications n'engage pas à participer à l'activité.
+            </p>
+        </div>
+
         <div class="input-container">
             <div class="subscription-mail-wrapper">
                 <label for="subscription-mail">Adresse e-mail</label>
@@ -10,13 +17,12 @@
         </div>
 
         <p class="text-sm">
-            En cliquant sur «&nbsp;S'inscrire&nbsp;», vous acceptez de recevoir des e-mails informatifs du Cercle de Mycologie de Bruxelles.
-            <br>
+            En cliquant sur «&nbsp;Créer un rappel e-mail&nbsp;», vous acceptez de recevoir des e-mails informatifs du Cercle de Mycologie de Bruxelles.
             Vous pouvez vous désinscrire à tout moment via le lien présent en bas de chaque e-mail.
         </p>
 
         <div class="bottom-btn-container display-row">
-            <button type="submit" class="cmb-btn">S'inscrire</button>
+            <button type="submit" class="cmb-btn">Créer un rappel e-mail</button>
             <button type="button" class="close-btn">Annuler</button>
         </div>
     </form>
