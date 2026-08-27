f5b8e1ae8b700c8d1ef6e6e30e5d0a95299eb781
Téo Derboven <teoderboven@outlook.com>
2026-08-27 18:52:03 +0200

    Subscription: remove old sendReminders

--- app/Http/Controllers/ActivityNotificationController.php
+++ app/Http/Controllers/ActivityNotificationController.php
@@ -4,17 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ActivityNotificationRegisterRequest;
 use App\Http\Requests\ActivityNotificationUnregisterRequest;
-use Carbon\Carbon;
-use App\Mail\ActivityReminder;
 use App\Http\Requests\GetActivityNotificationSubscriptionsRequest;
 use App\Models\Activity;
 use App\Models\ActivityNotificationSubscription;
 use App\Models\MailSubscriber;
 use App\Services\SubscriberMailService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
 
 /**
@@ -163,77 +159,5 @@ class ActivityNotificationController extends Controller{
             'activityIds' => $activityIds,
         ]);
     }
-
-	/**
-	 * Sends activity reminders to subscribers.
-	 * Only sends reminders if the date of the activity is close.
-	 * A reminder is usually sent 7 days before an activity at 2pm and 2 days before an activity at 10am.
-	 */
-	public function sendReminders(): void{
-		$subscriptions = ActivityNotificationSubscription::with('subscriber')
-		->where('first_reminder_sent', false)
-		->orWhere('second_reminder_sent', false)
-		->get();
-
-		$now = Carbon::now();
-
-		foreach ($subscriptions as $subscription){
-			$activityDate = $subscription->activity->start_date;
-
-			if($subscription->second_reminder_sent || $now->greaterThanOrEqualTo($activityDate)){
-				continue; // nothing to send
-			}
-
-			$firstReminderDate = $activityDate->copy()->subDays(7)->setTime(14, 0);
-			$secondReminderDate = $activityDate->copy()->subDays(2)->setTime(10, 0);
-
-			// check second reminder
-			if($now->greaterThanOrEqualTo($secondReminderDate)){
-				Mail::to($subscription->subscriber->email)->send(new ActivityReminder($subscription->activity, $subscription->subscriber));
-
-				$subscription->second_reminder_sent = true;
-				$subscription->save();
-			}
-			// check first reminder
-			else if($now->greaterThanOrEqualTo($firstReminderDate) && !$subscription->first_reminder_sent){
-				Mail::to($subscription->subscriber->email)->send(new ActivityReminder($subscription->activity, $subscription->subscriber));
-
-				$subscription->first_reminder_sent = true;
-				$subscription->save();
-			}
-		}
-	}
-
-	/**
-	 * Hourly task: send reminder to subscribers.
-	 * Secured by the .env CRON_SECRET_KEY.
-	 * @param Request $req
-	 * @param string $key the pretending secret key
-	 * @return string returns 'done'
-	 */
-	public function secureSendReminders(Request $req, string $key){
-		$taskName = 'send reminders';
-		$reqIp = $req->ip();
-		Log::notice("task $taskName accessed by $reqIp");
-
-		$secretKey = config('app.cron_secret_key');
-
-		if(empty($secretKey)){
-			throw new \RuntimeException('CRON_SECRET_KEY is not set in the configuration.');
-		}
-
-		if(empty($key) || $key !== $secretKey){
-			Log::warning("task $taskName has refused $reqIp. key=$key");
-			abort(403, 'Unauthorized');
-		}
-
-		// key is valid, execute task
-
-		Log::notice("task $taskName has validate $reqIp, task executed");
-
-		$this->sendReminders();
-
-		return 'done';
-	}
 
 }
--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::get('/excursions', function () {
 })->name('excursions');
 // TODO: exclude cookie middleware for /excursions/xx/abc
 //		 ->withoutMiddleware([CheckCookiesAccepted::class])
-
-// Tasks (web cron) routes
-
-Route::middleware('throttle:3,1')->group(function () { // limit at 3 access/minute
-    Route::get('/tasks/send-activity-reminder-mails/{key}', [ActivityNotificationController::class, 'secureSendReminders']);
-});
+//
+//// Tasks (web cron) routes
+//
+//Route::middleware('throttle:3,1')->group(function () { // limit at 3 access/minute
+//    Route::get('/tasks/send-activity-reminder-mails/{key}', [ActivityNotificationController::class, 'secureSendReminders']);
+//});
 
 // Special routes
 
