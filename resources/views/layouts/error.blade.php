004be1e592e3da4c92397621445a4eee10ad8663
Téo Derboven <teoderboven@outlook.com>
2026-09-09 21:18:34 +0200

    Style: fix error override

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,78 +1,80 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=.7">
-	<meta name="robots" content="noindex">
-	<title>Erreur @yield('code') - CMB</title>
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="shortcut icon" type="image/x-icon" href="/assets/common/img/icon.ico">
-	<link rel="shortcut icon" type="image/png" sizes="64x64" href="/assets/common/img/icon64.png">
-	<link rel="shortcut icon" type="image/png" sizes="256x256" href="/assets/common/img/icon256.png">
-	<link rel="apple-touch-icon" sizes="256x256" href="/assets/common/img/icon256.png">
-	<link rel="stylesheet" href="/assets/common/css/header.css">
-	<link rel="stylesheet" href="/assets/common/css/footer.css">
-	@vite(['resources/scss/app.scss'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=.7">
+    <meta name="robots" content="noindex">
+    <title>Erreur @yield('code') - CMB</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/common/img/icon.ico">
+    <link rel="shortcut icon" type="image/png" sizes="64x64" href="/assets/common/img/icon64.png">
+    <link rel="shortcut icon" type="image/png" sizes="256x256" href="/assets/common/img/icon256.png">
+    <link rel="apple-touch-icon" sizes="256x256" href="/assets/common/img/icon256.png">
+    <link rel="stylesheet" href="/assets/common/css/header.css">
+    <link rel="stylesheet" href="/assets/common/css/footer.css">
+    @vite(['resources/scss/app.scss'])
 </head>
 <body>
 @includeWhen(config('app.debug'), 'common.partials.debug')
-	<header>
-		<div>
-			<div id="topTitle">
-				<a href="{{ route('home', [], false) }}" id="title">
-					<img srcset="/assets/common/img/icon256.png 256w,
+<header>
+    <div>
+        <div id="topTitle">
+            <a href="{{ route('home', [], false) }}" id="title">
+                <img srcset="/assets/common/img/icon256.png 256w,
 								 /assets/common/img/icon1500.png 1500w"
-						  sizes="(max-width: 420px) 1500px,
+                     sizes="(max-width: 420px) 1500px,
 								 256px"
-					 		src="/assets/common/img/icon256.png" alt="">
-					<h1>
-						<span class="word">Cercle</span><span class="particle"> de </span><span class="word">Mycologie</span><span class="particle"> de </span><span class="word">Bruxelles</span>
-					</h1>
-				</a>
-			</div>
-		</div>
-	</header>
-	<main>
-		<section id="error-container">
-			<p class="code">Erreur @yield('code')</p>
-			<p class="reason">
-				@yield('reason')
-			</p>
-			<p class="comment">
-				<q>@yield('comment')</q>
-			</p>
-			@hasSection('instruction')
-				<p class="instruction">
-					@yield('instruction')
-				</p>
-			@endif
-		</section>
-		<section id="choices">
-			<div>
-				<a href="{{ route('home', [], false) }}" class="home-btn cmb-btn">Aller à la page d'accueil</a>
-			</div>
-			<p>
-				Vous cherchez quelque chose de spécifique?<br>
-				Consultez notre liste de pages populaires ci-dessous&nbsp;:
-			</p>
-			<nav>
-				<ul>
-					<li><a href="{{ route('activities', [], false) }}">prochaines activités</a></li>
-					<li><a href="{{ route('publications', [], false) }}">publications</a></li>
-					<li><a href="{{ route('excursions', [], false) }}/">Historique des excursions</a></li>
-					<li><a href="{{ route('member', [], false) }}">Devenir membre</a></li>
-					<li><a href="{{ route('parasites', [], false) }}">les champignons parasites des plantes</a></li>
-				</ul>
-			</nav>
-		</section>
-	</main>
-	<footer>
-		<div>
-			<div id="footer">
-				<span id="copy">&copy; {{ date('Y') }} - Cercle de Mycologie de Bruxelles</span>
-			</div>
-		</div>
-	</footer>
+                     src="/assets/common/img/icon256.png" alt="">
+                <h1>
+                    <span class="word">Cercle</span><span class="particle"> de </span><span class="word">Mycologie</span><span class="particle"> de </span><span class="word">Bruxelles</span>
+                </h1>
+            </a>
+        </div>
+    </div>
+</header>
+<main>
+    <div id="error-wrapper">
+        <section id="error-container">
+            <p class="code">Erreur @yield('code')</p>
+            <p class="reason">
+                @yield('reason')
+            </p>
+            <p class="comment">
+                <q>@yield('comment')</q>
+            </p>
+            @hasSection('instruction')
+                <p class="instruction">
+                    @yield('instruction')
+                </p>
+            @endif
+        </section>
+        <section id="choices">
+            <div>
+                <a href="{{ route('home', [], false) }}" class="home-btn cmb-btn">Aller à la page d'accueil</a>
+            </div>
+            <p>
+                Vous cherchez quelque chose de spécifique?<br>
+                Consultez notre liste de pages populaires ci-dessous&nbsp;:
+            </p>
+            <nav>
+                <ul>
+                    <li><a href="{{ route('activities', [], false) }}">prochaines activités</a></li>
+                    <li><a href="{{ route('publications', [], false) }}">publications</a></li>
+                    <li><a href="{{ route('excursions', [], false) }}/">Historique des excursions</a></li>
+                    <li><a href="{{ route('member', [], false) }}">Devenir membre</a></li>
+                    <li><a href="{{ route('parasites', [], false) }}">les champignons parasites des plantes</a></li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+</main>
+<footer>
+    <div>
+        <div id="footer">
+            <span id="copy">&copy; {{ date('Y') }} - Cercle de Mycologie de Bruxelles</span>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
