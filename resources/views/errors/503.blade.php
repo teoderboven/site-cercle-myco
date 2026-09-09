c7f3932a9d495ce895e617ceb064989af189e91f
Téo Derboven <teoderboven@outlook.com>
2026-09-09 15:46:40 +0200

    Errors: add 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.error')
+
+@section('code', '503')
+@section('reason', "Service temporairement indisponible ou en maintenance.")
+@section('comment', "Le serveur prend un court instant pour faire sa maintenance ou reprendre son souffle sous la charge.")
+@section('instruction', 'Veuillez patienter quelques minutes, le site sera bientôt de retour.')
