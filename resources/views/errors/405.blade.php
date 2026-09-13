513b17cd611803067743a10214d265908b168ff9
Téo Derboven <teoderboven@outlook.com>
2026-09-13 21:21:21 +0200

    Error: add 405

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.error')
+
+@section('code', '405')
+@section('reason', "Méthode non autorisée.")
+@section('comment', "Cette méthode de cueillette est formellement interdite ici.")
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::view('/champi-parasite-des-plantes', 'pages.champi-parasite.index')->name
 
 Route::view('/error/403', 'errors.403');
 Route::view('/error/404', 'errors.404');
+Route::view('/error/405', 'errors.405');
 Route::view('/error/500', 'errors.500');
 Route::view('/error/503', 'errors.503');
 
